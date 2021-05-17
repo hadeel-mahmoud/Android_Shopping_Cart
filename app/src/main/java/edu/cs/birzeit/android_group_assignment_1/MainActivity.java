c718a8b17828fbc109add554791f02371b5a90c2
Lama <lamashabana228@gmail.com>
2021-05-17 21:33:12 +0300

    add activity

--- app/src/main/java/edu/cs/birzeit/android_group_assignment_1/MainActivity.java
+++ app/src/main/java/edu/cs/birzeit/android_group_assignment_1/MainActivity.java
@@ -2,7 +2,9 @@ package edu.cs.birzeit.android_group_assignment_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
         //test
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Login_onClick(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
