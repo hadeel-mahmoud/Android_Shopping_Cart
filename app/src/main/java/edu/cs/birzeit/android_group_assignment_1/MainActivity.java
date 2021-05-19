41f1ffe148e9cc949d0526325da9cf7d01c99e7d
Lama <lamashabana228@gmail.com>
2021-05-19 19:26:39 +0300

    sign in - simple ui

--- app/src/main/java/edu/cs/birzeit/android_group_assignment_1/MainActivity.java
+++ app/src/main/java/edu/cs/birzeit/android_group_assignment_1/MainActivity.java
@@ -5,17 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editEmail, editPassword;
+    Button btnSignIn;
+
+    String URL = "https://10.0.3.2/rest/users.php";
+
+
+
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //test
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editEmail = (EditText) findViewById(R.id.editEmail);
+        editPassword = (EditText) findViewById(R.id.editPassword);
+       // btnSignIn = (Button) findViewById(R.id.btnSignIn);
+
+
     }
 
-    public void Login_onClick(View view) {
+    public void signIn_onClick(View view) {
         Intent intent = new Intent(this, MainActivity2.class);
         startActivity(intent);
     }
--- app/src/main/java/edu/cs/birzeit/android_group_assignment_1/MainActivity2.java
+++ app/src/main/java/edu/cs/birzeit/android_group_assignment_1/MainActivity2.java
@@ -1,14 +1,154 @@
 package edu.cs.birzeit.android_group_assignment_1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.provider.SyncStateContract;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity2 extends AppCompatActivity {
-
+  //  private SQLiteHelper db;
+    private ListView list ;
+  //  CustomAdapter adapter;
+    public MainActivity mainActivity=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
     }
-}
+    private String processRequest(String restUrl) throws UnsupportedEncodingException {
+//        String title = edtBookTitle.getText().toString();
+//        String category = edtBookCategory.getText().toString();
+//        String pages = edtBookPages.getText().toString();
+//
+//        String data = URLEncoder.encode("title", "UTF-8")
+//                + "=" + URLEncoder.encode(title, "UTF-8");
+//
+//        data += "&" + URLEncoder.encode("cat", "UTF-8") + "="
+//                + URLEncoder.encode(category, "UTF-8");
+//
+//        data += "&" + URLEncoder.encode("pages", "UTF-8")
+//                + "=" + URLEncoder.encode(pages, "UTF-8");
+
+        String text = "";
+        BufferedReader reader=null;
+
+        // Send data
+        try
+        {
+
+            // Defined URL  where to send data
+            URL url = new URL(restUrl);
+
+            // Send POST data request
+
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+//            wr.write( data );
+            wr.flush();
+
+            // Get the server response
+
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = "";
+
+            // Read Server Response
+            while((line = reader.readLine()) != null)
+            {
+                // Append server response in string
+                sb.append(line + "\n");
+            }
+
+
+            text = sb.toString();
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+
+                reader.close();
+            }
+
+            catch(Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        // Show response on activity
+        return text;
+
+
+
+    }
+
+    private class SendPostRequest extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return processRequest(urls[0]);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            return "";
+        }
+        @Override
+        protected void onPostExecute(String result) {
+
+            Toast.makeText(MainActivity2.this, result, Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void print_onClick(View view) {
+        String restUrl = "http://10.0.2.2:84/rest/addbook.php";
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.INTERNET},
+                    123);
+
+        } else{
+            SendPostRequest runner = new SendPostRequest();
+            runner.execute(restUrl);
+        }
+    }
+
+    }
+
+
